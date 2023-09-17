c5c46ecf442ff322d6c4cc7db6f2eec9d0ebae98
torinunna <torinunna9588@gmail.com>
2023-09-17 23:37:10 +0900

    Initial Commit

---
+++ DadokDadok/DadokDadokApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadokDadokApp.swift
+//  DadokDadok
+//
+//  Created by YUJIN KWON on 2023/09/17.
+//
+
+import SwiftUI
+
+@main
+struct DadokDadokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
