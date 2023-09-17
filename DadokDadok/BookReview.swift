f764e30426d2538eb017961fc5d0c8de5deb4a39
torinunna <torinunna9588@gmail.com>
2023-09-17 23:57:52 +0900

    create BookReview

---
+++ DadokDadok/BookReview.swift
@@ -0,0 +1,25 @@
+//
+//  BookReview.swift
+//  DadokDadok
+//
+//  Created by YUJIN KWON on 2023/09/17.
+//
+
+import Foundation
+
+struct BookReview: Identifiable {
+    let id: UUID = UUID()
+    var title: String
+    var imageName: String
+    var date: String
+    var review: String
+}
+
+extension BookReview {
+    static let sampleData: [BookReview] = [
+        BookReview(title: "code", imageName: "book", date: "2023-04-23", review: "this is amazing"),
+        BookReview(title: "heart", imageName: "heart", date: "2023-05-23", review: "I love it"),
+        BookReview(title: "pen", imageName: "pen", date: "2023-09-15", review: "I want to write")
+    ]
+}
+
