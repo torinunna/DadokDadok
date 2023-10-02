c040ddcbf1e2d25986581faa13061f3d76b4f1fc
torinunna <torinunna9588@gmail.com>
2023-10-02 15:18:26 +0900

    add datePicker to ReviewEditView

--- DadokDadok/ReviewEditView.swift
+++ DadokDadok/ReviewEditView.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 struct ReviewEditView: View {
     @Binding var review: BookReview
+    @State private var selectedDate = Date()
+    
+    init(review: Binding<BookReview>) {
+        _review = review
+        _selectedDate = State(initialValue: Self.dateFormatter.date(from: review.wrappedValue.date) ?? Date())
+    }
+    
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 15) {
             HStack(spacing: 20) {
                 Image(systemName: review.imageName)
                     .resizable()
@@ -22,10 +34,17 @@ struct ReviewEditView: View {
             }
             .padding()
             
+            DatePicker("읽은 날짜", selection: $selectedDate, displayedComponents: .date)
+                .environment(\.locale, Locale.init(identifier: "ko-KR"))
+            
             TextEditor(text: $review.review)
                 .border(.gray.opacity(0.2), width: 3)
         }
         .padding()
+        
+        .onChange(of: selectedDate, perform: { newDate in
+            review.date = Self.dateFormatter.string(from: newDate)
+        })
     }
 }
 
