981dd17a25a9da1f0a5cc451459287e9ec18e7b7
torinunna <torinunna9588@gmail.com>
2024-02-13 22:52:57 +0900

    modify EditReviewView

--- DadokDadok/Review List/EditReviewView.swift
+++ DadokDadok/Review List/EditReviewView.swift
@@ -16,37 +16,43 @@ struct EditReviewView: View {
             HStack(spacing: 15) {
                 fetchImage(url: vm.bookReview.book.image)
                 
-                VStack(alignment: .leading, spacing: 5) {
+                VStack(alignment: .leading, spacing: 3) {
                     Text(vm.bookReview.book.title)
                         .font(.system(size: 14, weight: .semibold))
+                        .padding(.bottom, 3)
                     Text("\(vm.bookReview.book.author) | \(vm.bookReview.book.publisher)")
                         .font(.system(size: 13))
                     Text(vm.bookReview.book.isbn)
                         .font(.system(size: 12))
                 }
-                Spacer()
             }
             
             DatePicker("읽은 날짜", selection: $vm.selectedDate, displayedComponents: .date)
                 .environment(\.locale, Locale.init(identifier: "ko-KR"))
             
             TextEditor(text: $vm.bookReview.review)
-                .border(.gray.opacity(0.2), width: 3)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.black.opacity(0.25), lineWidth: 1)
+                )
         }
-        .padding(.horizontal)
+        .padding()
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 Button("저장") {
                     vm.saveChanges()
                     presentationMode.wrappedValue.dismiss()
                 }
+                .disabled( vm.bookReview.review.isEmpty)
             }
         }
     }
     
     func fetchImage(url: String) -> some View {
         AsyncImage(url: URL(string: url)) { image in
-            image.resizable()
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
         } placeholder: {
             Image(systemName: "book")
         }
@@ -54,3 +60,10 @@ struct EditReviewView: View {
     }
 }
 
+
+struct EditReviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        let vm = EditReviewViewModel(bookReview:  BookReview.sampleData.first!)
+        EditReviewView(vm: vm)
+    }
+}
