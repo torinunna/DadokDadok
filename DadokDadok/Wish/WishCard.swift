4fd2436da6095516ba9161347322d2cb8006680f
torinunna <torinunna9588@gmail.com>
2024-06-13 13:34:11 +0900

    add a rectangle as a divider to WishCard

--- DadokDadok/Wish/WishCard.swift
+++ DadokDadok/Wish/WishCard.swift
@@ -15,38 +15,51 @@ struct WishCard: View {
     
     
     var body: some View {
-        HStack(alignment: .center, spacing: 15) {
-            BookImageView(wish: wish)
-                .frame(width: 60, height: 80)
-                .onTapGesture {
-                    isImageMagnified = true
-                }
-            
-            VStack(alignment: .leading, spacing: 3) {
-                Text(wish.book.title)
-                    .font(.system(size: 14.0, weight: .semibold))
-                    .lineLimit(5)
-                    .multilineTextAlignment(.leading)
-                    .padding(.vertical, 3)
+        VStack {
+            HStack(alignment: .center, spacing: 15) {
+                BookImageView(wish: wish)
+                    .frame(width: 60, height: 80)
+                    .onTapGesture {
+                        isImageMagnified = true
+                    }
                 
-                if !wish.book.author.isEmpty {
-                    Text(wish.book.author)
-                        .font(.system(size: 12.0, weight: .medium))
+                VStack(alignment: .leading, spacing: 3) {
+                    Text(wish.book.title)
+                        .font(.system(size: 14.0, weight: .semibold))
+                        .lineLimit(5)
+                        .multilineTextAlignment(.leading)
+                        .padding(.vertical, 3)
+                    
+                    if !wish.book.author.isEmpty {
+                        Text(wish.book.author)
+                            .font(.system(size: 12.0, weight: .medium))
+                    }
+                    
+                    if !wish.book.publisher.isEmpty {
+                        Text(wish.book.publisher)
+                            .font(.system(size: 12.0, weight: .medium))
+                    }
                 }
                 
-                if !wish.book.publisher.isEmpty {
-                    Text(wish.book.publisher)
-                        .font(.system(size: 12.0, weight: .medium))
+                Spacer()
+                
+                if !wish.book.link.isEmpty {
+                    Button {
+                        openURL(URL(string: wish.book.link)!)
+                    } label: {
+                        Image(systemName: "info.circle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 20)
+                    }
+                    .foregroundStyle(ColorManager.accentColor)
+                    .buttonStyle(PlainButtonStyle())
                 }
-            }
-            
-            Spacer()
-            
-            if !wish.book.link.isEmpty {
+                
                 Button {
-                    openURL(URL(string: wish.book.link)!)
+                    vm.send(action: .toggleFavorite(wish))
                 } label: {
-                    Image(systemName: "info.circle")
+                    (wish.isFavorite ? Image(systemName: "heart.fill") : Image(systemName: "heart"))
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 20)
@@ -55,16 +68,9 @@ struct WishCard: View {
                 .buttonStyle(PlainButtonStyle())
             }
             
-            Button {
-                vm.send(action: .toggleFavorite(wish))
-            } label: {
-                (wish.isFavorite ? Image(systemName: "heart.fill") : Image(systemName: "heart"))
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 20)
-            }
-            .foregroundStyle(ColorManager.accentColor)
-            .buttonStyle(PlainButtonStyle())
+            Rectangle()
+                .frame(height: 1)
+                .foregroundStyle(ColorManager.cardColor)
         }
         .background(ColorManager.backgroundColor)
         .popover(isPresented: $isImageMagnified) {
